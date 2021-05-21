cf202bc3d5312093b2d7531843d38b008eaa0055
Vitali Shadrin <vitali.shadrin.consultant@nielsen.com>
2021-05-21 14:22:38 +0300

    Added resize updates

--- src/comparison/app/controller/TestOptionsController.java
+++ src/comparison/app/controller/TestOptionsController.java
@@ -4,11 +4,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class TestOptionsController extends ControllerHelper implements Initializable {
@@ -21,10 +25,13 @@ public class TestOptionsController extends ControllerHelper implements Initializ
 
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
-        TitledPane tilePane = new TitledPane();
-        tilePane.setText("Hello");
-        accordion.setExpandedPane(tilePane);
-
+        options.getProperties().stringPropertyNames().stream().sorted().forEach(name -> {
+            TextField textField = new TextField();
+            TitledPane titledPane = new TitledPane(name + "(" + options.getProperties().getProperty(name) + ")" , textField);
+            textField.setText(options.getProperties().getProperty(name));
+            titledPane.setFont(Font.font(null, FontWeight.BOLD, 12));
+            accordion.getPanes().add(titledPane);
+        });
         accordion.prefWidthProperty().bind(pane.widthProperty());
         scrollPane.prefWidthProperty().bind(pane.widthProperty());
         scrollPane.prefHeightProperty().bind(pane.heightProperty());
